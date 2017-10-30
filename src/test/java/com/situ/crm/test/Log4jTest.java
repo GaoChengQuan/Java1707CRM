9f50922b0c737ad28df02e325fcd6d2fdfadd64f
GaoChengQuan <529390053@qq.com>
2017-10-30 11:23:51 +0800

    log4j配置

---
+++ src/test/java/com/situ/crm/test/Log4jTest.java
@@ -0,0 +1,16 @@
+package com.situ.crm.test;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+public class Log4jTest {
+
+	@Test
+	public void test() {
+		Logger logger = Logger.getLogger(Log4jTest.class);
+		logger.error("Error");
+		logger.warn("Warn");
+		logger.info("Info");
+		logger.debug("Debug");
+	}
+}
